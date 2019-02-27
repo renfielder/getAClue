7240507c94a51fc982687ba4b7de37daa94a1ffb
amystphn <47902205+amystphn@users.noreply.github.com>
2019-02-27 18:33:33 -0500

    Update rooms.h

--- project2/rooms.h
+++ project2/rooms.h
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "items.h"
+#include "avatar.h"
 #define YES 1
 #define NO 0
 
 //function prototype declarations
-struct Room* newRoom(char* description, Item* items, room *north, room *south, room *east, room *west,room *up, room *down);
+struct room* newRoom(char* description, Item* items, room *north, room *south, room *east, room *west,room *up, room *down);
 void go(room* other);
 void lock(room* tbl);
+void printItems(room* tbp);
+
 
 struct room{
   char *description;
   struct Item *items;
-  struct Room *North;
-  struct Room *South;
-  struct Room *East;
-  struct Room *West;
-  struct Room *Up;
-  struct Room *Down;
+  struct room *North;
+  struct room *South;
+  struct room *East;
+  struct room *West;
+  struct room *Up;
+  struct room *Down;
   int locked=NO;
-  
 };
 
-typedef struct room Room;
+typedef struct room room;
 
 
 
