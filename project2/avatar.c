11f0cff6121aaf24e91e26a09f9d0f8438f7c954
nuhaBoston <39391126+nuhaBoston@users.noreply.github.com>
2019-02-26 19:25:56 -0500

    Update avatar.c

--- project2/avatar.c
+++ project2/avatar.c
@@ -2,10 +2,36 @@
 #include <stdlib.h> 
 #include <string.h> 
 #include "items.h"
+#include "rooms.h"
 
 typedef struct Avatar{
   char* name;
-  Item *inventory;
-  
-
+  Item* inventory;
+  Item* hat;
 };
+
+Avatar* avatar(char* name, Item* inventory, Item* hat){
+  Avatar* avatar; 
+  avatar = (struct Avatar*) malloc(sizeof(struct Avatar));
+  avatar -> name = name;
+  avatar -> inventory = inventory;
+  avatar -> hat = hat; 
+  return avatar;
+}
+
+char* getAvatarName(Avatar* person){
+  return person->name;}
+
+Item* getInventory(Avatar* person){
+  return person->inventory;
+}
+
+Item* getHat(Avatar* person){
+  return person->hat;}
+
+
+
+
+
+
+
