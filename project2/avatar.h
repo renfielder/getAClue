dda46e02b509ef90b322a02c664680622bccd666
nuhaBoston <39391126+nuhaBoston@users.noreply.github.com>
2019-03-01 12:14:33 -0500

    just made some changes items.h

--- project2/avatar.h
+++ project2/avatar.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include "rooms.h"
-#include "items.h"
+#include "item.h"
 #define YES 1
 #define NO 0 
 
--- project2/rooms.h
+++ project2/rooms.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "items.h"
+#include "item.h"
 #include "avatar.h"
 #define YES 1
 #define NO 0
