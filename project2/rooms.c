d988c287315e967b92d86ad4ff46392a1a03b6e1
nuhaBoston <39391126+nuhaBoston@users.noreply.github.com>
2019-02-28 23:35:33 -0500

    Update rooms.c

--- project2/rooms.c
+++ project2/rooms.c
@@ -16,3 +16,34 @@ void room_exit_north(struct Room* current, struct Room* other){
   current->North=other;
 }
 
+
+//function to exit current room to room in specified direction
+void go(room* direction){
+  if(current->direction != NULL){
+    current=direction;
+  }
+  else printf("There is nothing here!");
+}
+
+//locks the room
+void lock(room* tbl){
+  tbl->locked=YES;
+}
+
+//prints the list of items in the room 
+void printItems(room* tbp){
+  
+}
+
+//free rooms
+void freeRooms(room* tbf){
+  free(kitchen);
+  free(ballroom);
+  free(billiard);
+  free(conservatory);
+  free(library);
+  free(study);
+  free(atrium);
+  free(lounge);
+  free(dining);
+}
