875ac2fc6e7beeebf5319913721d62a8c099b455
amystphn <47902205+amystphn@users.noreply.github.com>
2019-03-02 13:08:04 -0500

    Update avatar.h

--- project2/avatar.h
+++ project2/avatar.h
@@ -13,21 +13,13 @@ typedef struct avatar{
   room* current;
 }Avatar;
 
+//function prototype declarations
 Avatar* avatar(char* name, Item* inventory, room* current);
-
-
 char* getAvatarName(Avatar* person);
-
 Item* getInventory(Avatar* person);
-
 char* getInventoryItemDescription(Avatar* person, char* variable);
-
-
-
 char* lookRoomDescription(room* current);
-
 Item* lookItemsinRoom(room* current);
-
 char* lookItemsinRoomDescription(room* current);
 
 #endif
