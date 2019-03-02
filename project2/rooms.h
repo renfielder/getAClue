9a3536d1869151ff8ef8ffd466aedb832cbc6c21
amystphn <47902205+amystphn@users.noreply.github.com>
2019-03-02 14:14:13 -0500

    Update rooms.h

--- project2/rooms.h
+++ project2/rooms.h
@@ -24,6 +24,5 @@ typedef struct room{
 room* newRoom(char* name, char* description, Item* items, room *north, room *south, room *east, room *west,room *up, room *down);
 room* go(room* direction, room* current);
 void lock(room* tbl);
-void printItems(room* tbp);
 
 #endif
